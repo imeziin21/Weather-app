8677b360207d6ac8a3c2d1003cebb583d6ba809c
imeziin21 <imeziin21@gmail.com>
2026-07-09 14:41:54 +0900

    weather model 구현

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/provider/weather_provider.dart';
 import 'package:weather_app/screen/home_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
   runApp(
     ChangeNotifierProvider(
         create: (BuildContext) => weatherprovider(),
--- lib/model/weather_models.dart
+++ lib/model/weather_models.dart
@@ -1,3 +1,22 @@
 class weathermodel {
+  late final String city;
+  late final double temp;
+  late final String description;
+  late final int humidity;
 
+  weathermodel({
+    required this.city,
+    required this.temp,
+    required this.description,
+    required this.humidity,
+  });
+
+  factory weathermodel.fromJson(Map<String, dynamic> json){
+    return weathermodel(
+      city: json['name'],
+      temp: json['main']['temp'].toDouble(),
+      description: json['weather'][0]['description'],
+      humidity: json['main']['humidity'],
+    );
+  }
 }
