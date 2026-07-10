50697078491214b8a7bcf634d803c7f7c07cb311
imeziin21 <imeziin21@gmail.com>
2026-07-10 09:54:43 +0900

    OpenWeather API 연동 기능 추가

--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -1,17 +1,18 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import '../model/weather_models.dart';
 
-class WeatherService {
+class weatherservice {
   final String apikey = dotenv.env['OPENWEATHER_API_KEY']!;
 
-  Future<Map<String,dynamic>> getweather(String city) async {
-    final url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=$dotenv.env['OPENWEATHER_API_KEY']!&units=metric&lang=kr");
+  Future<weathermodel> getweather(String city) async {
+    final url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apikey&units=metric&lang=kr");
 
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      return jsonDecode(response.body);
+      return weathermodel.fromJson(jsonDecode(response.body));
     } else {
       throw Exception("날씨 정보를 불러오지 못했습니다.");
     }
