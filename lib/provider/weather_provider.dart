e0e1a80b64dd138220356762b0f21615e2522f15
imeziin21 <imeziin21@gmail.com>
2026-07-10 09:52:54 +0900

    날씨 상태관리 provider 추가

--- lib/provider/weather_provider.dart
+++ lib/provider/weather_provider.dart
@@ -1,6 +1,25 @@
-import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_app/model/weather_models.dart';
+import 'package:weather_app/service/weather_service.dart';
 
-class weatherprovider extends ChangeNotifier{
 
+class weatherprovider extends ChangeNotifier {
+  final weatherservice service = weatherservice();
+  weathermodel? weather;
+  bool isLoading = false;
+
+  Future<void> catchweather(String city) async {
+    try {
+      isLoading = true;
+      notifyListeners();
+      weather = await service.getweather(city);
+    }
+    catch(e){
+      print(e);
+    }
+    finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
 }
